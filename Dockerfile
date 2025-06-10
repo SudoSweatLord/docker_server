f303b0964986984f607126657e6d6b6df1ffbd84
SudoSweatLord <fahrenberger.k@web.de>
2025-06-10 11:08:16 +0200

    add: enviroment

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM debian:stable-slim
 
-# COPY source destination
+ENV PORT=8991
+
 COPY goserver /bin/docker_server
 
-#Automatically starts the server process in the container when it's running
 CMD ["/bin/docker_server"]
+
--- main.go
+++ main.go
@@ -1,41 +1,43 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"time"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "time"
 )
 
 func main() {
-	m := http.NewServeMux()
+    m := http.NewServeMux()
+    m.HandleFunc("/", handlePage)
 
-	m.HandleFunc("/", handlePage)
+    // Read port from environment
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8999"
+    }
 
-	const port = "8010"
-	srv := http.Server{
-		Handler:      m,
-		Addr:         ":" + port,
-		WriteTimeout: 30 * time.Second,
-		ReadTimeout:  30 * time.Second,
-	}
+    srv := http.Server{
+        Handler:      m,
+        Addr:         ":" + port,
+        WriteTimeout: 30 * time.Second,
+        ReadTimeout:  30 * time.Second,
+    }
 
-	// this blocks forever, until the server
-	// has an unrecoverable error
-	fmt.Println("server started on ", port)
-	err := srv.ListenAndServe()
-	log.Fatal(err)
+    fmt.Println("server started on port", port)
+    err := srv.ListenAndServe()
+    log.Fatal(err)
 }
 
 func handlePage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(200)
-	const page = `<html>
+    w.Header().Set("Content-Type", "text/html")
+    w.WriteHeader(200)
+    const page = `<html>
 <head></head>
 <body>
-	<p> Hello from Docker! I'm a Go server. </p>
+    <p> Hello from Docker! I'm a Go server. </p>
 </body>
-</html>
-`
-	w.Write([]byte(page))
+</html>`
+    w.Write([]byte(page))
 }
